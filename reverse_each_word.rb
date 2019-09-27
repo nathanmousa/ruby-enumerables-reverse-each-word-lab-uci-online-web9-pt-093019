2fb8299bfe638ce816260c66b0d5a564a6863c90
Nathan Mousa <nathanmousa1@gmail.com>
2019-09-27 00:27:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(sentence)
-  sentence.reverse
-  return sentence
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
